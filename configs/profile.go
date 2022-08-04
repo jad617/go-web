6c537a5c41f4eba25574a96f06ecac3f58e0b373
krassimir <wass.asmar@gmail.com>
2022-08-04 00:05:57 -0400

    add mechanism to fetch envfile from root of project

--- configs/profile.go
+++ configs/profile.go
@@ -3,23 +3,33 @@ package configs
 import (
 	"log"
 	"os"
-	"path/filepath"
+	"path"
+	"runtime"
 
 	"github.com/joho/godotenv"
 )
 
 func GetActiveProfile() {
-	getWd, _ := os.Getwd()
-	baseDir := filepath.Dir(getWd)
+	_, goFileExecuted, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Println("function runtime.Caller() failed to get the current execution filename")
+	}
+
+	projectRootDir := path.Join(path.Dir(goFileExecuted), "..")
+
+	err := os.Chdir(projectRootDir)
+	if err != nil {
+		panic(err)
+	}
 
 	switch os.Getenv("GO_ENV") {
 	case "local":
-		err := godotenv.Load(baseDir + "/.env-local")
+		err := godotenv.Load(projectRootDir + "/.env-local")
 		if err != nil {
 			log.Fatal(err)
 		}
 	case "pipeline":
-		err := godotenv.Load(baseDir + "/.env-pipeline")
+		err := godotenv.Load(projectRootDir + "/.env-pipeline")
 		if err != nil {
 			log.Fatal(err)
 		}
