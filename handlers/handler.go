50bf516487d0419a8065c719b5e89e053b2158af
jelasmar <jelasmar@pop-os.localdomain>
2022-08-06 00:13:04 -0400

    removed gin.Default(), added gin.New() with gin.Recoverry and gin.Logger() only set when gin.Mode == "debug"

--- handlers/handler.go
+++ handlers/handler.go
@@ -66,7 +66,18 @@ func containsFile(fetchedTemplateFiles []string, requestedPathHTML string) bool 
 func Handler() *gin.Engine {
 	conf := configs.FetchVars()
 
-	r := gin.Default()
+	// Creates a router without any middleware by default
+	r := gin.New()
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	if gin.Mode() == "debug" {
+		r.Use(gin.Logger())
+	}
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	r.Use(gin.Recovery())
 
 	err = r.SetTrustedProxies(nil)
 	if err != nil {
